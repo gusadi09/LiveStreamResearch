965d9293bec6292ac52d87362bd5601d6d5c0351
Agus Adi Pranata <agus.adi@icehousecorp.com>
2022-03-29 16:35:27 +0700

    Initial Commit

---
+++ LiveStreamResearch/LiveStreamResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveStreamResearchApp.swift
+//  LiveStreamResearch
+//
+//  Created by Gus Adi on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveStreamResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
